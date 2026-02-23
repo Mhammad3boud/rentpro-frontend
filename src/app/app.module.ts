5a9f3d0028e648803dce0d0f6e11b349714d54e5
Mhammad3boud <mmohammadaboud06@gmail.com>
2026-02-23 14:19:53 +0800

    fix: resolve Ionic element errors by converting LeaseSelectorComponent to standalone

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,19 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LeaseSelectorComponent } from './components/lease-selector/lease-selector.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 import { Router } from '@angular/router';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SidebarComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
+    LeaseSelectorComponent,
     HttpClientModule
   ],
   providers: [
--- src/app/components/lease-selector/lease-selector.component.ts
+++ src/app/components/lease-selector/lease-selector.component.ts
@@ -1,7 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  IonLabel,
+  IonSelect,
+  IonSelectOption
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-lease-selector',
+  standalone: true,
+  imports: [CommonModule, IonLabel, IonSelect, IonSelectOption],
   templateUrl: './lease-selector.component.html',
   styleUrls: ['./lease-selector.component.scss']
 })
